96376034bd141e093f3ff9d5f518840cdafa164a
Ritik Kothari <ritikkothari912@gmail.com>
2021-12-13 15:06:16 +0530

    Update note.php

--- product/note.php
+++ product/note.php
@@ -70,7 +70,7 @@
             <p class="greet">THANK YOU<br>FOR BUYING</p>
         </div>
         <footer style="position: absolute; right: 0; left: 0; bottom: 0;" class="page-footer">
-                <div class="container"><p style="color: white; font-family: Calibri">© 2019, Polaris, Inc. All rights reserved.<br>Developed by <a style="text-decoration: none" class="devs" href="https://www.facebook.com/smitnawar.streak">ScarletStreak</a>, <a style="text-decoration: none" class="devs" href="https://www.facebook.com/siddharth.kothari.750">SoulTrinity</a> and <a style="text-decoration: none" class="devs" href="https://www.facebook.com/profile.php?id=100009769879471">ArchAngel</a>.</p></div>
+                <div class="container"><p style="color: white; font-family: Calibri">© 2019, Polaris, Inc. All rights reserved.<br>Developed by <a style="text-decoration: none" class="devs" href="https://www.facebook.com/smitnawar">Smit Nawar</a>, <a style="text-decoration: none" class="devs" href="https://www.facebook.com/siddharth.kothari.750">SoulTrinity</a> and <a style="text-decoration: none" class="devs" href="https://www.facebook.com/profile.php?id=100009769879471">ArchAngel</a>.</p></div>
         </footer>
     </body>
 </html>
